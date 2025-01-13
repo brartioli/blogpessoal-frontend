ee0d4552aa35888c3d8437308b960548efa3aece
brartioli <brunaartioli.ba@gmail.com>
2025-01-13 14:34:45 -0300

    feat: add pages, home and index.html

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,38 @@
+function Home() {
+    return (
+        <>
+            <div style={{
+                width: "100vw",
+                display: "flex",
+                justifyContent: "center"
+            }}>
+                <div>
+                    <div style={{
+                         width: "80vw",
+                         display: "flex",
+                         flexDirection: "column",
+                         alignItems: "center"
+                    }}>
+                        <h2>Seja bem-vindes!!</h2>
+                        <p>Expresse suas opniões e pesamentos aqui!</p>
+                    </div>
+
+                    <div style={{
+                         width: "80vw",
+                         display: "flex",
+                         flexDirection: "column",
+                         alignItems: "center"
+                    }}>
+                        <img 
+                            src="https://i.imgur.com/VpwApCU.png" 
+                            alt="Imagem da Página Home" 
+                            width="400px"
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
