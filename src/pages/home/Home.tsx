d7c6d609aabef30dcd1a5ffd733a91ad1278e1e3
brartioli <brunaartioli.ba@gmail.com>
2025-01-15 11:27:23 -0300

    cfeat: add postcss.config.js and tailwind.config.js

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,28 +1,14 @@
 function Home() {
     return (
         <>
-            <div style={{
-                width: "100vw",
-                display: "flex",
-                justifyContent: "center"
-            }}>
+            <div className="w-screen flex justify-center">
                 <div>
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center"
-                    }}>
+                    <div className="mt-5 max-w-7xl flex flex-col items-center"> 
                         <h2>Seja bem-vindes!!</h2>
                         <p>Expresse suas opniões e pesamentos aqui!</p>
                     </div>
 
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center"
-                    }}>
+                    <div className="mt-5 max-w-7xl flex flex-col items-center">
                         <img 
                             src="https://i.imgur.com/VpwApCU.png" 
                             alt="Imagem da Página Home" 
