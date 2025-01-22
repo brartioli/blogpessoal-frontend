8ea5d5ae5038d9dbf388829f7a1d47ebcae761d1
brartioli <brunaartioli.ba@gmail.com>
2025-01-22 14:08:46 -0300

    feat: add folder postagens contain: cardpostagens, deletarpostagens, formpostagens, listapostagens, modalpostagens

---
+++ src/components/postagens/cardpostagens/CardPostagens.tsx
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom'
+import Postagem from '../../../models/Postagem'
+
+interface CardPostagensProps {
+    postagem: Postagem
+}
+
+function CardPostagens({ postagem }: CardPostagensProps) {
+    return (
+        <div className='border-green-900 border 
+            flex flex-col rounded overflow-hidden justify-between'>
+
+            <div>
+                <div className="flex w-full bg-green-700 py-2 px-4 items-center gap-4">
+                    <img src={postagem.usuario?.foto}
+                        className='h-12 rounded-full' alt={postagem.usuario?.nome} />
+                    <h3 className='text-lg font-thin text-center uppercase'>
+                        {postagem.usuario?.nome}
+                    </h3>
+                </div>
+                <div className='p-4 '>
+                    <h4 className='text-lg font-thin uppercase'>{postagem.titulo}</h4>
+                    <p>{postagem.texto}</p>
+                    <p>Tema: {postagem.tema?.descricao}</p>
+                    <p>Data: {new Intl.DateTimeFormat(undefined, { dateStyle: 'full', timeStyle: 'medium', }).format(new Date(postagem.data))}</p>
+                </div>
+            </div>
+            <div className="flex">
+                <Link to={`/editarpostagem/${postagem.id}`}
+                    className='w-full text-slate-100 bg-green-400 hover:bg-green-800 flex items-center justify-center py-2'>
+                    <button>Editar ♻</button>
+                </Link>
+                <Link to={`/deletarpostagem/${postagem.id}`}
+                    className='text-white bg-red-400 hover:bg-red-700 w-full flex items-center justify-center'>
+                    <button>Deletar ❌</button>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default CardPostagens
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,11 @@
+import ListaPostagens from "../../components/postagens/listapostagens/ListaPostagens"
+import ModalPostagem from "../../components/postagens/modalpostagem/ModalPostagem"
+
 function Home(){
     return(
         <>
-            <div className= "bg-green-700 flex justify-center">
-                <div className="container grid grid-cols-2 text-white">
+            <div className= "bg-lime-100 flex justify-center">
+                <div className="container grid grid-cols-2 text-green-800">
                     <div className="flex flex-col gap-4 items-center justify-center py-4">
                         <h2 className="text-5xl font-thin">
                             Seja bem-Vinde!!
@@ -11,14 +14,14 @@ function Home(){
                              Expresse aqui seus pensamentos e opniões
                         </p>
                         <div className="flex justify-around gap-4">
-                            <div className="rounded text-white border-white border-solid border-2 py-2 px-4 font-thin">
-                                Nova postagem
+                            <div className="flex justify-around gap-4">
+                                 <ModalPostagem />
                             </div>
 
                         </div>
 
                     </div>
-                    <div className="flex justify-center">
+                    <div  id="imagem" className="flex justify-center">
                         <img src="https://i.imgur.com/fyfri1v.png" alt="Imagem Página Home" className='w-2/3'/>
 
                     </div>
@@ -26,6 +29,8 @@ function Home(){
                 </div>
 
             </div>
+
+             <ListaPostagens />
         </>
     )
 }
