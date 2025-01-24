fba51965b2d3baa5b822440e04f604b750eb9672
brartioli <brunaartioli.ba@gmail.com>
2025-01-24 12:00:38 -0300

    feat: add utils e todas páginas atualizadas com os ToastAlertas

--- src/components/postagens/listapostagens/ListaPostagens.tsx
+++ src/components/postagens/listapostagens/ListaPostagens.tsx
@@ -5,6 +5,7 @@ import Postagem from "../../../models/Postagem";
 import { AuthContext } from "../../../contexts/AuthContext";
 import { buscar } from "../../../services/Service";
 import { DNA } from "react-loader-spinner";
+import { ToastAlerta } from "../../../utils/ToastAlerta";
 
 function ListaPostagens(){
 
@@ -31,7 +32,7 @@ function ListaPostagens(){
 
     useEffect(() => {
         if(token === ''){
-            alert('Você precisa estar logado!')
+            ToastAlerta('Você precisa estar logado!', "info")
             navigate('/');
         }
     }, [token])
